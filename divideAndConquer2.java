b648d2e1390e250859aef329e3790ba1fe977bcb
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-08-20 20:20:29 +0300

    Create divideAndConquer2.java

---
+++ divideAndConquer2.java
@@ -0,0 +1,20 @@
+package nick.pack;
+
+
+import java.io.*;
+
+public class Main {
+    //Напишите рекурсионныю функцию для подсчета колличества эллементов в массиве
+        public static void main(String[] args) throws IOException {
+            int[] arr = {2,4,3,4};
+            System.out.println(count(arr));
+        }
+        public static int count(int[] arr){
+            if (arr.length == 0){
+                return 0;
+            } else {
+                int[] temp = new int[arr.length - 1];
+                return 1 + count(temp);
+            }
+        }
+}
